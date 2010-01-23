e4afc2ea27f640f97744de83307410f9981fd2ce
Prasanna Gautam <pgautam@hal9000.(none)>
2010-01-22 22:45:23 -0500

    updates on new version of facebook-cleaner

--- facebook-cleaner/fbcleaner.js
+++ facebook-cleaner/fbcleaner.js
@@ -1,12 +1,16 @@
 function remove(element) {
-	element.parentNode.removeChild(element);
+	if (element != null) 
+		element.parentNode.removeChild(element);
 }
 
 var suggestionBox = document.getElementById('pagelet_pymkbox');
-var adBox =document.getElementById('pagelet_adbox');
+var homeAdBox =document.getElementById('pagelet_adbox');
 var connectBox = document.getElementById('pagelet_connectbox');
+var sidebarAds = document.getElementById('sidebar_ads');
 
 remove(suggestionBox);
-remove(adBox);
+remove(homeAdBox);
 remove(connectBox);
+if (sidebarAds)
+	sidebarAds.style.visibility = 'hidden';
 
