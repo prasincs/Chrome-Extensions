aace3713b20a2e186d97eeea0d3bc2318449a27c
Prasanna Gautam <pgautam@hal9000.(none)>
2010-01-31 03:19:13 -0500

    added popup and options, along with automatically refreshing the page after options are chosen

--- facebook-cleaner/fbcleaner.js
+++ facebook-cleaner/fbcleaner.js
@@ -3,14 +3,64 @@ function remove(element) {
 		element.parentNode.removeChild(element);
 }
 
-var suggestionBox = document.getElementById('pagelet_pymkbox');
-var homeAdBox =document.getElementById('pagelet_adbox');
-var connectBox = document.getElementById('pagelet_connectbox');
-var sidebarAds = document.getElementById('sidebar_ads');
 
-remove(suggestionBox);
-remove(homeAdBox);
-remove(connectBox);
-if (sidebarAds)
-	sidebarAds.style.visibility = 'hidden';
+function removeItems() {
+    var opts = {
+        'hide_suggestionBox' :false
+    };
+
+    var suggestionBox = document.getElementById('pagelet_pymkbox');
+    var homeAdBox =document.getElementById('pagelet_adbox');
+    var connectBox = document.getElementById('pagelet_connectbox');
+    var sidebarAds = document.getElementById('sidebar_ads');
+
+    chrome.extension.sendRequest (
+    {
+      "type" : "config",
+      "keys" : ["hide_suggestionBox", "hide_homeAdBox", "hide_connectBox", "hide_sidebarAds"]
+    },
+    function (data) {
+       if (data["hide_suggestionBox"]==="true") {
+        remove(suggestionBox);
+        }
+       if (data["hide_homeAdBox"]==="true") {
+        remove(homeAdBox);
+        }
+        if (data["hide_connectBox"]==="true") {
+            remove(connectBox);
+        }
+        if (data["hide_sidebarAds"]==="true") {
+            if (sidebarAds)
+                sidebarAds.style.visibility= 'hidden';
+        }else {
+            if (sidebarAds)
+                sidebarAds.style.visibility= 'visible';
+        }
+    });
+    
+}
+
+
+function toggle(element){
+    if (element==='suggestionBox'){
+        window.localStorage.hide_suggestionBox = document.optionsform.hide_suggestionBox.checked;
+    }else if (element === 'homeAdBox') {
+        window.localStorage.hide_homeAdBox = document.optionsform.hide_homeAdBox.checked;
+    }else if (element === 'connectBox') {
+        window.localStorage.hide_connectBox = document.optionsform.hide_connectBox.checked;
+    }else if (element === 'sidebarAds') {
+        window.localStorage.hide_sidebarAds = document.optionsform.hide_sidebarAds.checked;
+    }
+}
+
+
+function refresh(){
+    window.location.reload();
+}
+
+
+if (location.href.indexOf("facebook.com")!= -1) {
+    chrome.extension.sendRequest({msg: "icon", href:location.href});
+    removeItems();
+}
 
